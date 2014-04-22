c3e41cd252715a560dadfd6619ee61d45762080b
Ryan Farrell <ryan.farrell@mcgarrybowen.com>
2014-04-22 16:31:15 -0400

    set up inhertance so both controllers use same data fetch method

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -10,17 +10,96 @@ var players = [];
 
 var homeTeam = 0;
 
-app.controller("stats", function($scope, $http, $route, $filter) {
+app.controller("pullData", function ($scope, $route, gameService) {
+
+	$scope.gameId = $route.current.params.gameId;
+
+	$scope.getGame = gameService.getGameInfo($scope.gameId);
+
+	$scope.getEvents = gameService.getEventInfo($scope.gameId);
 	
+	$scope.players = [],
+	
+	$scope.events,
+	
+	$scope.teams,
+	
+	$scope.filterTeams = [];
+	
+	var _matchData = {};
+	
+	$scope.getGame.success(function(data) {
+		
+		//Populate global var MatchData w/ match data from service
+		$.extend(_matchData, data);
+
+		//Assign a home team
+		homeTeam = data.SoccerDocument.Team[0]["@attributes"].uID.split('t')[1];
+
+		//Populate $scope.players
+		$.each(data.SoccerDocument.Team, function(i,e) {
+
+			for (var x = 0; x < e.Player.length; x++) {
+
+				var pl = e.Player[x];
+
+				var playerData = [];
+
+				playerData.push(pl["@attributes"].uID);
+
+				playerData.push(pl.PersonName.First);
+
+				playerData.push(pl.PersonName.Last);
+
+				playerData.push(pl["@attributes"].Position);
+
+				$scope.players.push(playerData);
+			}
+		});//End of Team loop
+		
+		$scope.teams = data.SoccerDocument.Team;
+		
+	});//End of getGame
+	
+	//populate $scope.events
+	$scope.getEvents.success(function(data) {
+
+		var _richEvents = [];
+
+		$.each(data.Game.Event, function(i,e) {
+
+			_richEvents.push(e);
+		});
+
+    $scope.events = _richEvents;
+	});
+});
+
+
+app.controller("stats", function ($scope, $http, $route, $filter, $controller) {
+	
+	//Inherit gets from pullData controller
+	$controller("pullData", {$scope: $scope});
+	
+	$scope.getEvents.success(function() {
+		
+	});
 	
 });
 
-app.controller("passmap", function ($scope, $http, $route, $filter, gameService){
+app.controller("passmap", function ($scope, $filter, $controller) {
+	
+	//Inherit gets from pullData controller
+	$controller("pullData", {$scope: $scope});
 
 	$scope.filterItem = {store: 0};
 
 	$scope.filterPlayers = {};
 
+	$scope.filterTeams = [];
+	
+	$scope.players = [];
+	
 	$scope.filterTeams = [];
 
 	$scope.filterTime = {
@@ -28,13 +107,6 @@ app.controller("passmap", function ($scope, $http, $route, $filter, gameService)
 		start : 0,
 		end : 90
 	}
-
-	$scope.gameId = $route.current.params.gameId;
-	
-	$scope.getGame = gameService.getGameInfo($scope.gameId).success(function() {
-	
-		$scope.getEvents = gameService.getEventInfo($scope.gameId);
-	});
 	
 	//$scope.game = gameService.getGameInfo($scope.gameId);
 
@@ -46,7 +118,25 @@ app.controller("passmap", function ($scope, $http, $route, $filter, gameService)
 	
 		e.preventDefault();
 		$passInfo.fadeOut();
-	})
+	});
+	
+	
+	$scope.getGame.success(function(data) {
+
+		//populate global var teams with array of team ids and team names
+		$.each(data.SoccerDocument.Team, function(i,v) {
+
+			var teamID = this["@attributes"].uID;
+
+			var teamName = this.Name;
+
+			var t = []
+
+			t.push(teamID, teamName);
+
+			$scope.filterTeams.push(t);
+		});
+	});
 
 	$scope.customFilter = function (data) {
 	
@@ -88,143 +178,83 @@ app.controller("passmap", function ($scope, $http, $route, $filter, gameService)
 	    else return false;
 	};
 	
-	$scope.getGame.success(function(data) {
-		
-			//Populate global var MatchData w/ match data from service
-		$.extend(MatchData, data);
+	
+	
+	//Populate Event data
+	$scope.getEvents.success(function(data) {
 
-		//Assign a home team
-		homeTeam = MatchData.SoccerDocument.Team[0]["@attributes"].uID.split('t')[1];
+		$scope.addClasses = function() {
 
-		//populate golbal var teams with array of team ids and team names
-		$.each(MatchData.SoccerDocument.Team, function(i,v) {
+			var c = "";
 
-			var teamID = this["@attributes"].uID;
+			var t = parseInt(this.event["@attributes"].type_id);
 
-			var teamName = this.Name;
+			if (t === 1) c += " pass";
 
-			var t = []
+				else if (t === 16) c += " shot goal"
 
-			t.push(teamID, teamName);
+				else if (shots.indexOf(t) !== -1) c += " shot miss";
 
-			$scope.filterTeams.push(t);
-		});
+			return c;
+		}
 
-		//populate golbal var player with array of player ids and player names
-		$.each(MatchData.SoccerDocument.Team, function(i,e) {
+		$scope.style = function(element) {
 
-			for (var x = 0; x < e.Player.length; x++) {
+			var style = "";
 
-				var pl = e.Player[x];
+			var arrowHead = '<div class="arrowhead"></div>';
 
-				var playerData = [];
+			var toCoords = getCoords(this.event);
 
-				playerData.push(pl["@attributes"].uID);
+			var atts = this.event["@attributes"];
 
-				playerData.push(pl.PersonName.First);
-
-				playerData.push(pl.PersonName.Last);
-
-				playerData.push(pl["@attributes"].Position);
-
-				players.push(playerData);
-			}
-		});
-		
-		//Populate Event data
-		$scope.getEvents.success(function(data) {
-
-			//Events
-			var eventsAttr = [];
-
-			//Event Qualifiers
-			var eventsQ = [];
-
-			var richEvents = [];
-
-			$.each(data.Game.Event, function(i,e) {
-
-				richEvents.push(e);
-			});
-
-			$scope.players = MatchData.SoccerDocument.Team;
-
-	    $scope.events = richEvents;
-
-			$scope.addClasses = function() {
-
-				var c = "";
-
-				var t = parseInt(this.event["@attributes"].type_id);
-
-				if (t === 1) c += " pass";
-
-					else if (t === 16) c += " shot goal"
-
-					else if (shots.indexOf(t) !== -1) c += " shot miss";
-
-				return c;
-			}
-
-			$scope.style = function(element) {
-
-				var style = "";
-
-				var arrowHead = '<div class="arrowhead"></div>';
-
-				var toCoords = getCoords(this.event);
-
-				var atts = this.event["@attributes"];
-
-				var fromCoords = {
-					left : atts.x,
-					top : atts.y
-				};
-
-				var w = pythagorize(toCoords, fromCoords);
-
-				var ang = calculateAngle(toCoords, fromCoords);
-
-				var awayColorOffset = (atts.team_id === homeTeam) ? 40 : 190;
-
-				ang = Math.round(ang);
-
-				//var rgbVar = pickColor(atts.min, atts.sec);d
-
-				var isSuccessOffset = (isSuccessfullAction(this.event["@attributes"])) ? 65 : 30;
-
-				return {
-					'width' : w + '%',
-					'-webkit-transform' : 'rotate(' + ang + 'deg)',
-					'left' : fromCoords.left + '%',
-					'top' : fromCoords.top + '%',
-					'background-color' : 'hsla('  + awayColorOffset + ', 80%,'  + isSuccessOffset + '%, 1)',
-					'border-color' : 'hsla('  + awayColorOffset + ', 80%,'  + isSuccessOffset + '%, 1)'
-				};
+			var fromCoords = {
+				left : atts.x,
+				top : atts.y
 			};
 
+			var w = pythagorize(toCoords, fromCoords);
 
-			$scope.showMoreInfo = function() {
+			var ang = calculateAngle(toCoords, fromCoords);
 
-				var ngThis = this.event["@attributes"],
-					moContent = '',
-					player = getPlayerNameFromId(players, ngThis.player_id),
-					outcomeInt = parseInt(ngThis.outcome),
-					outcome = (isSuccessfullAction(ngThis)) ? 'completed' : 'failed'
+			var awayColorOffset = (atts.team_id === homeTeam) ? 40 : 190;
 
-				moContent += '<strong class="time">' + ngThis.min + '&rsquo;' + ngThis.sec + '&rdquo;' + '</strong><br/>';
-				moContent += player[1] + " " + player[2] + '<br/>';
-				moContent += outcome + " " + eventAppendix[ngThis.type_id - 1].toLowerCase();
+			ang = Math.round(ang);
 
-				$(event.target.parentNode).children().removeClass('selected');
+			//var rgbVar = pickColor(atts.min, atts.sec);d
 
-				$(event.target).addClass('selected');
+			var isSuccessOffset = (isSuccessfullAction(this.event["@attributes"])) ? 65 : 30;
 
-				$passInfo.fadeIn().find('.content').html(moContent);
-			}
-		});
-	});
+			return {
+				'width' : w + '%',
+				'-webkit-transform' : 'rotate(' + ang + 'deg)',
+				'left' : fromCoords.left + '%',
+				'top' : fromCoords.top + '%',
+				'background-color' : 'hsla('  + awayColorOffset + ', 80%,'  + isSuccessOffset + '%, 1)',
+				'border-color' : 'hsla('  + awayColorOffset + ', 80%,'  + isSuccessOffset + '%, 1)'
+			};
+		};
 
+		$scope.showMoreInfo = function() {
+			
+			var ngThis = this.event["@attributes"],
+				moContent = '',
+				player = getPlayerNameFromId($scope.players, ngThis.player_id),
+				outcomeInt = parseInt(ngThis.outcome),
+				outcome = (isSuccessfullAction(ngThis)) ? 'completed' : 'failed'
+
+			moContent += '<strong class="time">' + ngThis.min + '&rsquo;' + ngThis.sec + '&rdquo;' + '</strong><br/>';
+			moContent += player[1] + " " + player[2] + '<br/>';
+			moContent += outcome + " " + eventAppendix[ngThis.type_id - 1].toLowerCase();
+
+			$(event.target.parentNode).children().removeClass('selected');
+
+			$(event.target).addClass('selected');
+
+			$passInfo.fadeIn().find('.content').html(moContent);
+		}
+	});	
+	
 	//instantiate slider
 	$( "#slider-range" ).slider({
 		range: true,
@@ -249,12 +279,8 @@ app.controller("passmap", function ($scope, $http, $route, $filter, gameService)
 
 	$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
 		" - $" + $( "#slider-range" ).slider( "values", 1 ) );
-	
 });
 
-// function passmap($scope, $http, $route, $filter) {
-// 	
-// }
 /*
 function timeline($scope, $http) {
 	
